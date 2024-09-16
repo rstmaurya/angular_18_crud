d9d46a94b558f8790e4fd8ea920ed9654576807c
rstmaurya <108594597+rstmaurya@users.noreply.github.com>
2024-09-17 04:00:18 +0530

    Your commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,87 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18_crud';
+
+  empObj :EmployeeModel =new EmployeeModel();
+  empList :EmployeeModel[]=[]
+  
+  empForm :FormGroup =new FormGroup({});
+  constructor(){
+
+    this.createForm()
+    const oldData =localStorage.getItem("EmpData");
+    if(oldData !== null){
+      const parseData=JSON.parse(oldData);
+      this.empList=parseData;
+  }
+}
+  createForm(){
+    this.empForm =new FormGroup({
+      empId :new FormControl(this.empObj.empId),
+      name :new FormControl(this.empObj.name),
+      city :new FormControl(this.empObj.city),
+      address :new FormControl(this.empObj.address),
+      contactNo :new FormControl(this.empObj.contactNo),
+      emailId :new FormControl(this.empObj.emailId),
+      pinCode :new FormControl(this.empObj.pinCode),
+      state :new FormControl(this.empObj.state),
+    })
+  }
+
+  onSave(){
+    debugger;
+    const oldData =localStorage.getItem("EmpData");
+    if(oldData !== null){
+      const parseData=JSON.parse(oldData);
+      this.empForm.controls['empId'].setValue(parseData.length +1)
+      this.empList.unshift(this.empForm.value)
+
+    }else{
+      this.empList.unshift(this.empForm.value);
+    }
+    localStorage.setItem("EmpData",JSON.stringify(this.empList))
+
+    this.empObj =new EmployeeModel();
+   this.createForm();
+  }
+
+  onEdit(item:EmployeeModel){
+    this.empObj =item;
+    this.createForm();
+
+  }
+
+  onUpdate(){
+  const record =this.empList.find(m=>m.empId==this.empForm.controls['empId'].value)
+   if(record !=undefined){
+    record.address=this.empForm.controls['address'].value
+    record.name=this.empForm.controls['name'].value
+    record.contactNo=this.empForm.controls['contactNo'].value
+   }
+   localStorage.setItem("EmpData",JSON.stringify(this.empList))
+   this.empObj =new EmployeeModel();
+   this.createForm();
+
+  }
+
+  onDelete(id:number){
+      const isDelete =confirm('Are u want to delete ?')
+      if(isDelete){
+        const index=this.empList.findIndex(m=>m.empId ==id)
+        this.empList.splice(index,1)
+        localStorage.setItem("EmpData",JSON.stringify(this.empList))
+
+      }
+  }
+  
 }
---
+++ src/app/model/Employee.ts
@@ -0,0 +1,22 @@
+export class EmployeeModel{
+ empId:number;
+ name:string;
+ city:string;
+ state:string;
+ emailId:string;
+ contactNo:string;
+ address:string;
+ pinCode:string;
+
+ constructor(){
+    this.address='';
+    this.city='';
+    this.contactNo='';
+    this.emailId='';
+    this.empId=0;
+    this.name='';
+    this.state='';
+    this.pinCode='';
+    
+ }
+}
